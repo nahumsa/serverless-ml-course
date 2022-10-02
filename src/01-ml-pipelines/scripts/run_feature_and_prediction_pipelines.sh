443f70e78b92985ab5bb9738b4cb1fa86c701857
Nahum Sá <nahumsaa@gmail.com>
2022-10-02 00:33:03 -0300

    add pipenv run to bash

--- src/01-ml-pipelines/scripts/run_feature_and_prediction_pipelines.sh
+++ src/01-ml-pipelines/scripts/run_feature_and_prediction_pipelines.sh
@@ -5,5 +5,5 @@ set -e
 
 cd src/01-ml-pipelines
 
-jupyter nbconvert --to notebook --execute iris_feature_pipeline.ipynb
-jupyter nbconvert --to notebook --execute iris_batch_inference_pipeline.ipynb
+pipenv run jupyter nbconvert --to notebook --execute iris_feature_pipeline.ipynb
+pipenv run jupyter nbconvert --to notebook --execute iris_batch_inference_pipeline.ipynb

6e7e982e512e558cf5b4738f26b48ee8008a2e7f
Nahum Sá <nahumsaa@gmail.com>
2022-10-02 00:07:36 -0300

    add jekyl theme

--- src/01-ml-pipelines/scripts/run_feature_and_prediction_pipelines.sh
+++
@@ -1,9 +0,0 @@
-# !/bin/bash
-
-# exit if there is an error
-set -e
-
-cd src/01-ml-pipelines
-
-jupyter nbconvert --to notebook --execute iris_feature_pipeline.ipynb
-jupyter nbconvert --to notebook --execute iris_batch_inference_pipeline.ipynb

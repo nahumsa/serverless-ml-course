3ba33481403b16b7f2c58d37fefa689810148f80
Nahum Rosa Cruz Sa <49600259+nahumsa@users.noreply.github.com>
2022-10-18 10:13:39 -0300

    fix script path

--- src/02_data_modeling_credit_card/scripts/run_fraud_feature_pipeline.sh
+++ src/02_data_modeling_credit_card/scripts/run_fraud_feature_pipeline.sh
@@ -5,5 +5,4 @@ set -e
 
 cd src/02_data_modeling_credit_card
 
-pipenv run jupyter nbconvert --to notebook --execute iris_feature_pipeline.ipynb
-pipenv run jupyter nbconvert --to notebook --execute iris_batch_inference_pipeline.ipynb
+pipenv run jupyter nbconvert --to notebook --execute 2_cc_feature_pipeline.ipynb
